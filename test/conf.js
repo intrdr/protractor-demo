2277e341dca6bc6b6c55e93564a2a5cd8ce1f086
intrdr <prihtnie@gmail.com>
2016-02-24 18:29:50 +0000

    Travis CI configuration and chrome.

--- test/conf.js
+++ test/conf.js
@@ -1,13 +1,3 @@
-// Start server.
-var express = require('express');
-var app = express();
-var util = require('util');
-
-app.use(express.static(__dirname));
-app.listen(3456);
-console.log('Server running at http://localhost:3456');
-
-
 // Tests for the calculator.
 exports.config = {
   directConnect: false,
